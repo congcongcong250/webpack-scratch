420cdc997f524fafc1cc5e44f435661dc62bb1fc
leewang <lirenxn@gmail.com>
2020-02-06 19:35:05 +1100

    Create 2 bundles in simplest way.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import _ from 'lodash';
+import printFn from './print.js';
 
 function component() {
-    const element = document.createElement('div');
-  
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    
-    return element;
-  }
-  
-  document.body.appendChild(component());
+  const element = document.createElement('div');
+
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  printFn();  
+  return element;
+}
+
+document.body.appendChild(component());
