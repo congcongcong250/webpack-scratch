2265053c9dcdc77f336990b846305c4b4fdafea2
leewang <lirenxn@gmail.com>
2020-02-06 16:18:36 +1100

    Clean up assets

--- src/index.js
+++ src/index.js
@@ -1,16 +1,10 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './icon.svg';
 
 function component() {
     const element = document.createElement('div');
   
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    // Add the image to our existing div.
-    const myIcon = new Image();
-    myIcon.src = Icon;
-
-    element.appendChild(myIcon);
+    
     return element;
   }
   
