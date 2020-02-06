4f0472762c24459049859d3d6e75dec9ce4a5935
leewang <lirenxn@gmail.com>
2020-02-06 19:48:28 +1100

    Use plugins to auto generate html * html-webpack-plugin to generate html, configured in webpack.config * clean-webpack-plugin to clean dist folder*

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
 
 module.exports = {
     entry: {
@@ -8,5 +11,11 @@ module.exports = {
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist')
-    }
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'Output Management'
+        })
+    ]
 }
